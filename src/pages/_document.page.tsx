3aa1c2360871cf4138b8338fd42c1e6eea06ccd6
no-gari <sofaissofa@icloud.com>
2023-09-06 12:53:12 +0900

    :lipstick: add font file

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -6,7 +6,13 @@ class MyDocument extends Document {
     return (
       <Html lang="ko">
           <Head>
-
+              <title>금붕어빵</title>
+              <link
+                  rel="stylesheet"
+                  as="style"
+                  href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css"
+              />
+              <meta charSet="utf-8" />
           </Head>
         <body>
           <Main />
--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -8,9 +8,7 @@ export const setGlobalStyles = css`
   ${emotionNormalize}
 
   body {
-    font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI',
-      'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',
-      sans-serif;
+    font-family: Pretendard, sans-serif;
     background-color: ${theme.colors.gray1};
     color: ${theme.colors.black};
   }
